6a54e4cee7383136dd52fc7aa61a222ea1e83090
Gabriel <54381049+gab706@users.noreply.github.com>
2024-01-18 14:09:37 +1100

    Update RouteManager.php

--- src/models/RouteManager.php
+++ src/models/RouteManager.php
@@ -1,87 +1,117 @@
 <?php
     session_regenerate_id();
-    include(__ROOT . '/src/models/Database.php');
+    include __ROOT . '/Model/Database.php';
 
     class RouteManager {
-        public $DB;
-        private $routes = array();
-        private $currentRoute;
-        private $identifier;
-        private $key;
+        public $db;
+        public $routes;
 
-        public function __construct() {
-            $this->DB = new Database(__DB_HOST, __DB_USER, __DB_PASSWORD, __DB_NAME);
-            $this->identifier = isset($_GET['key']) ? explode('.', $_GET['key'])[0] : null;
-            $this->key = isset($_GET['key']) ? explode('.', $_GET['key'])[1] : null;
-        }
+        public function __construct($routes) {
+            $this->db = new Database(__DB_HOST, __DB_USER, __DB_PASSWORD, __DB_NAME);
+            $this->routes = array('current' => null, 'available' => array());
+            $_SESSION['hiddenKeys'] = array();
 
-        public function addRoute($endpoint, $file) {
-            $this->routes[$endpoint] = $file;
+            if (isset($_REQUEST['key']) && strpos($_REQUEST['key'], '.') !== false) 
+                $_SESSION['key'] = $_REQUEST['key'];
+
+            foreach ($routes as $route => $file)
+                $this->routes['available'][$route] = $file;
         }
 
         public function run() {
-            foreach ($this->routes as $parent => $value) {
-                $urlsSnipped = $this->snipRequest($parent);
+            if (!isset($this->routes['available'][$this->choppedURL()]))
+                return $this->reject('Unknown Endpoint ' . $this->choppedURL(), 404);
 
-                if (!array_key_exists('/'. join('/', $urlsSnipped[1]), $this->routes))
-                    $this->rejectRequest('Unknown Endpoint', 404);
+            $this->routes['current'] = $this->db->query("SELECT * FROM API_endpoints WHERE endpoint = ? AND method = ?", $this->choppedURL(), $_SERVER['REQUEST_METHOD'])->fetchArray();
 
-                if (join('/', $urlsSnipped[0]) !== join('/', $urlsSnipped[1]))
-                    continue;
+            if (!$this->routes['current'])
+                return $this->reject('Error: Endpoint not in Database OR POST request needed', 500);
 
-                $this->currentRoute = $this->DB->query("SELECT * FROM API_endpoints WHERE url = ?", '/' . join('/', $urlsSnipped[1]))->fetchArray();
-
-                if (!count($this->currentRoute))
-                    $this->rejectRequest('Error: Endpoint not in Database', 500);
-
-                if ($this->currentRoute['permission'])
-                    $this->isKeyValid();
-
-                include_once(__ROOT . '/' . $value);
-
-                if (isset($_GET['r']))
-                    header('Location: ' . $_GET['r']);
-
-                exit();
+            if ($this->routes['current']['permission']) {
+                $this->securityCheck();
+                
+                $routeKeys = $this->db->query("SELECT name, permission FROM API_data WHERE endpoint = ?", $this->choppedURL())->fetchAll();
+                
+                foreach ($routeKeys as $routeKey => &$routeValue) 
+                    if (!($_SESSION['key']['data_permission'] & $routeValue['permission'])) 
+                        array_push($_SESSION['hiddenKeys'], $routeValue['name']);
             }
-        }
 
-        private function snipRequest($endpoint): array {
-            $request = strtok(rtrim(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL), '/'), '?');
-
-            $parts = explode('/', $endpoint);
-            $url_parts = explode('/', $request);
-            array_shift($parts);
-            array_shift($url_parts);
-
-            return array($parts, $url_parts);
-        }
-
-        private function isKeyValid() {
-            if ($this->key === null)
-                $this->rejectRequest('Missing one or more fields [key]', 400);
-
-            $_SESSION['KEY'] = $this->DB->query("SELECT * FROM API_keys WHERE identifier = ?", $this->identifier)->fetchArray();
-
-            if (!count($_SESSION['KEY']) || !password_verify($this->key, $_SESSION['KEY']['key']))
-                $this->rejectRequest('Invalid API Key', 403);
-
-            if (!($_SESSION['KEY']['permission'] & $this->currentRoute['permission']))
-                $this->rejectRequest('You cannot access this endpoint', 403);
-
-            if ($_SESSION['KEY']['disabled'])
-                $this->rejectRequest('Your key has been disabled, please contact an Admin', 403);
-
-            $this->DB->query("UPDATE API_keys SET requests = requests + 1 WHERE identifier = ?", $this->identifier);
-        }
-
-        public function rejectRequest($reason, $error) {
-            http_response_code($error);
-            echo json_encode(array(
-                'success' => false,
-                'reason' => $reason,
-                'error' => $error
-            ));
+            include_once __ROOT . '/' . $this->routes['available'][$this->choppedURL()];
             exit();
+        }
+        
+        
+        private function choppedURL() {
+            $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+            if ($path !== null) {
+                $segments = array_filter(explode('/', trim($path, '/')), fn($s) => strpos($s, '=') === false);
+                return count($segments) <= 2 ? '/' . implode('/', $segments) : '/';
+            }
+        
+            return '/';
+        }
+
+        private function securityCheck() {
+            if (!isset($_SESSION['key']))
+                return $this->reject('Missing one or more fields [key]', 400);
+            
+            $splitKey = explode('.', $_SESSION['key']);
+            $currentRoute = $this->routes['current'];
+
+            $_SESSION['key'] = $this->db->query("SELECT * FROM API_keys WHERE identifier = ?", $splitKey[0])->fetchArray();
+
+            if (!isset($_SESSION['key']) || !password_verify($splitKey[1], $_SESSION['key']['secret']))
+                return $this->reject('Invalid Key Used', 403);
+
+            $_SESSION['key']['secret'] = $splitKey[1];
+
+            $keyPermission = $_SESSION['key']['endpoint_permission'];
+            $currentPermission = $currentRoute['permission'];
+                
+            if (!($keyPermission & $currentPermission))
+                return $this->reject('You cannot access this endpoint', 403);
+                
+            if ($_SESSION['key']['disabled'])
+                return $this->reject('API Key Disabled', 403);
+
+            $keyType = $_SESSION['key']['type'];
+            $currentType = $currentRoute['type'];
+
+            if (!((($keyType == 1 && $currentType == 1) || ($keyType == 0 && $currentType == 0)) || ($keyType == 2 || $currentType == 2)))
+                return $this->reject('You cannot access this endpoint', 400);
+
+            if ($keyType === 1 && !(isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'], 'api.techtu.com.au') !== false))
+                return $this->reject('Invalid Key Used', 403);
+        }
+
+
+        public function reject($reason, $code) {
+            http_response_code($code);
+            echo json_encode([
+                'success' => false,
+                'reason' => $reason
+            ]);
+            exit();
+        }
+        
+        public function removeHiddenVariables($data) {
+            if (is_array($data)) {
+                foreach ($data as $key => $value) {
+                    if (is_array($value)) {
+                        $data[$key] = $this->removeHiddenVariables($value);
+                    } elseif (in_array($key, $_SESSION['hiddenKeys'])) {
+                        unset($data[$key]);
+                    }
+                }
+            } elseif (is_object($data)) {
+                foreach ($data as $key => $value) {
+                    if (in_array($key, $_SESSION['hiddenKeys'])) {
+                        unset($data->$key);
+                    }
+                }
+            }
+            return $data;
         }
     }
